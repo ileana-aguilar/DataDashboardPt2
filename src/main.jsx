6d99d3bdb48d97ba2121cd37cca650331085df71
Ileana Aguilar <73076457+ileana-aguilar@users.noreply.github.com>
2024-04-04 17:28:13 -0400

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -45,7 +45,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/bookdetails:bookKey" element={<BookDetail />} />
+      <Route path="/bookdetails:bookKey" element={<DetailView />} />
     </Routes>
 
     </BrowserRouter>
